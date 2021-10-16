c2620f35a7117f4bc5c3e6cbd7e7ba6f0993b9d8
DigantaD <diganta.aimlos@gmail.com>
2021-10-16 05:53:50 +0530

    initial run file for downloading data and splitting beats

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+pip3 install --upgrade spleeter
+sudo apt install unzip
+pip3 install --upgrade llvmlite
+gdown 'https://drive.google.com/uc?id=10NsxnANXKtn7g9urTuxDp3-pC-O_sK47&export=download'
+unzip data.zip -d .
+rm data.zip
+spleeter separate -o . ./genres_original/hiphop/hiphop.00000.wav
